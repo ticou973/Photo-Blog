e7f66d6f85bda100c62d0392a9aa2db053ce97cd
Thierry Couilleault <thierryc973@gmail.com>
2018-03-14 21:34:18 +0100

    Design page Login

--- app/src/main/java/com/example/thierrycouilleault/blogphoto/LoginActivity.java
+++ app/src/main/java/com/example/thierrycouilleault/blogphoto/LoginActivity.java
@@ -4,14 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
+
     }
 }
