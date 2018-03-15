8d89c6c7075d91f9d1619aafd3898227257260a5
Thierry Couilleault <thierryc973@gmail.com>
2018-03-15 10:09:55 +0100

    ajout du menu

--- app/src/main/java/com/example/thierrycouilleault/blogphoto/LoginActivity.java
+++ app/src/main/java/com/example/thierrycouilleault/blogphoto/LoginActivity.java
@@ -2,15 +2,114 @@ package com.example.thierrycouilleault.blogphoto;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button mLoginBtn, mRegBtn;
+    private EditText mEmailText, mPasswordText;
+
+    private FirebaseAuth mAuth;
+
+    private ProgressBar loginProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        mLoginBtn = findViewById(R.id.login_btn);
+        mRegBtn = findViewById(R.id.login_reg_btn);
+        mEmailText = findViewById(R.id.login_email);
+        mPasswordText = findViewById(R.id.login_password);
+        loginProgress = findViewById(R.id.login_progress);
+
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                String loginEmail = mEmailText.getText().toString();
+                String loginPass = mPasswordText.getText().toString();
+
+                if (!TextUtils.isEmpty(loginEmail) && !TextUtils.isEmpty(loginPass)) {
+
+                    loginProgress.setVisibility(View.VISIBLE);
+
+                    mAuth.signInWithEmailAndPassword(loginEmail, loginPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()){
+
+                                sendtoMain();
+
+
+                            } else {
+
+                                String errorMessage = task.getException().getMessage();
+                                Toast.makeText(LoginActivity.this, "Error : " + errorMessage, Toast.LENGTH_SHORT).show();
+                            }
+
+                            loginProgress.setVisibility(View.INVISIBLE);
+                        }
+                    });
+
+
+                }else {
+
+                    
+
+                }
+
+            }
+        });
+
 
     }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null){
+
+            sendtoMain();
+
+        }
+
+    }
+
+
+    private void sendtoMain() {
+
+        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
+
+    }
+
+
 }
--- app/src/main/java/com/example/thierrycouilleault/blogphoto/MainActivity.java
+++ app/src/main/java/com/example/thierrycouilleault/blogphoto/MainActivity.java
@@ -3,15 +3,65 @@ package com.example.thierrycouilleault.blogphoto;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mCurrentUser;
+
+    private android.support.v7.widget.Toolbar mainToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
+
+        mainToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
+        getSupportActionBar().setTitle("Photo Blog");
+
+
+
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mCurrentUser = mAuth.getCurrentUser();
+
+
+        if (mCurrentUser == null) {
+
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+
+
+        }else {
+
+
+
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+
+
     }
 }
